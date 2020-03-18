cdfc32fec729c48d3d0e836dff9e73a5f8ee0196
César Carruitero <ccarruitero@protonmail.com>
2020-03-18 10:40:34 -0500

    in AuthRoute, set token in redux state

--- src/components/AuthRoute.jsx
+++ src/components/AuthRoute.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setAuth } from '../reducers/authSlice'
 
-const AuthRoute = ({ children, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => (localStorage.getItem('authToken') ? (
-      children
-    ) : (
-      <Redirect
-        to={{ pathname: '/login', state: { from: props.location } }}
-      />
-    ))}
-  />
-);
+const AuthRoute = ({ children, ...rest }) => {
+  const dispatch = useDispatch();
+
+  const isAuthenticated = () => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      dispatch(setAuth({ token }));
+    }
+    return !!token;
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => (isAuthenticated() ? (
+        children
+      ) : (
+        <Redirect
+          to={{ pathname: '/login', state: { from: props.location } }}
+        />
+      ))}
+    />
+  );
+};
 
 export default AuthRoute;
