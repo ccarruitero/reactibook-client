11d2358c8a8c62919904fe6cd513b455ef1c29a7
César Carruitero <ccarruitero@protonmail.com>
2020-03-18 09:50:42 -0500

    add methods for post endpoints in reactibookAPI

--- src/api/reactibookAPI.js
+++ src/api/reactibookAPI.js
@@ -1,25 +1,40 @@
-export const login = async (data) => {
-  const url = `${process.env.REACT_APP_API_URL}/auth`;
-
-  const result = await fetch(url, {
-    method: 'post',
+const request = async ({
+  path, method, data, token,
+}) => {
+  const url = `${process.env.REACT_APP_API_URL}/${path}`;
+  const response = await fetch(url, {
+    method,
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(data),
-  }).then((res) => res.json());
-  return result;
+  })
+    .then((res) => res.json())
+    .catch((error) => console.log(`Error: ${error}`));
+  return response;
 };
 
-export const signUp = async (data) => {
-  const url = `${process.env.REACT_APP_API_URL}/users`;
+export const login = async (data) => request({
+  path: 'auth', method: 'post', data,
+});
 
-  const result = await fetch(url, {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  }).then((res) => res.json());
-  return result;
-};
+export const signUp = async (data) => request({
+  path: 'users', method: 'post', data,
+});
+
+export const createPost = async (data, token) => request({
+  path: 'posts', method: 'post', data, token,
+});
+
+export const updatePost = async (postId, data, token) => request({
+  path: `posts/${postId}`, method: 'put', data, token,
+});
+
+export const deletePost = async (postId, token) => request({
+  path: `posts/${postId}`, method: 'delete', token,
+});
+
+export const listPosts = async (token) => request({
+  path: 'posts', method: 'get', token,
+});
