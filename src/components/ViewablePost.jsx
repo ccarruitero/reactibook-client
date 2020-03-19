1c74524ea8a00c7676e58ff0b98a9512ba628b56
César Carruitero <ccarruitero@protonmail.com>
2020-03-19 18:00:49 -0500

    some style improvements in ViewablePost

--- src/components/ViewablePost.jsx
+++ src/components/ViewablePost.jsx
@@ -2,36 +2,53 @@ import React from 'react';
 import {
   Paper,
   Button,
+  Typography,
+  Box,
 } from '@material-ui/core';
 import StyledBox from  './shared/StyledBox';
 import {
   Edit,
   Delete,
 } from '@material-ui/icons';
+import { styled } from '@material-ui/core/styles';
+
+const StyledPaper = styled(Paper) ({
+  margin: '5px 0',
+  padding: '0.5rem',
+});
+
+const Heading = styled(Typography) ({
+  position: 'left',
+  'font-size': '14px',
+});
 
 const ViewablePost = ({ post, onEdit, onDelete }) => {
   return (
     <StyledBox>
-      {post.sharedWith}
-      <Paper>
+      <StyledPaper elevation={3}>
+        <Heading color="textSecondary" gutterBottom>
+          {post.sharedWith}
+        </Heading>
         {post.text}
-      </Paper>
-      <Button
-        variant='contained'
-        color='primary'
-        startIcon={<Edit />}
-        onClick={onEdit}
-      >
-        Edit
-      </Button>
-      <Button
-        variant='contained'
-        color='secondary'
-        startIcon={<Delete />}
-        onClick={onDelete}
-      >
-        Delete
-      </Button>
+        <Box>
+          <Button
+            variant='contained'
+            color='primary'
+            startIcon={<Edit />}
+            onClick={onEdit}
+          >
+            Edit
+          </Button>
+          <Button
+            variant='contained'
+            color='secondary'
+            startIcon={<Delete />}
+            onClick={onDelete}
+          >
+            Delete
+          </Button>
+        </Box>
+      </StyledPaper>
     </StyledBox>
   );
 };
